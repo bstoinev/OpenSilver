bb14b12ef453f3725dc630e32c4c84d6f59b5789
Bozhidar Stoyneff <bozhidar.stoinev@windowslive.com>
2021-08-26 16:24:23 +0300

    ScrollViewer inner div display set to contents

--- src/Runtime/Runtime/System.Windows.Controls/ScrollViewer.cs
+++ src/Runtime/Runtime/System.Windows.Controls/ScrollViewer.cs
@@ -567,6 +567,7 @@ namespace Windows.UI.Xaml.Controls
 
             object innerDiv;
             var innerDivStyle = INTERNAL_HtmlDomManager.CreateDomElementAppendItAndGetStyle("div", outerDiv, this, out innerDiv);
+            innerDivStyle.display = "contents";
             innerDivStyle.position = "relative";
 
             // Note: the "height" and "width" of the innerDiv are handled in the methods "INTERNAL_ApplyHorizontalSettings" and "INTERNAL_ApplyVerticalSettings".
